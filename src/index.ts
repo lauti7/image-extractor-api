6afb7023a7ab884d58ceb8516bfbecf07abe4cdf
Lautaro Bustos <lautigbustos@gmail.com>
2021-08-25 13:15:52 -0300

    removed routes from index file

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
-import getAllImages from './services/scrapping';
-
+import scrappingAPI from './routes/scrapping';
+import downloadAPI from './routes/download';
 dotenv.config();
 
 const app = express();
@@ -12,17 +12,8 @@ app.get('/api/status', (req: Request, res: Response) => {
   res.send('Ok');
 });
 
-app.post('/api/scrape', (req: Request, res: Response) => {
-  const weburl = req.body.weburl;
-  getAllImages(weburl)
-    .then((imgs) => {
-      res.status(200).json({ images: imgs });
-    })
-    .catch((error) => {
-      console.log(error);
-      res.json({ images: [], error: true });
-    });
-});
+scrappingAPI(app);
+downloadAPI(app);
 
 app.listen(process.env.PORT, () => {
   console.log('Running');
