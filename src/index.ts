4f251aeeb95931706f016ddeab2eb717b1627fcb
Lautaro Bustos <lautigbustos@gmail.com>
2021-08-26 12:17:27 -0300

    added cors

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import scrappingAPI from './routes/scrapping';
 import downloadAPI from './routes/download';
 dotenv.config();
@@ -7,6 +8,8 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
+
+app.use(cors());
 
 app.get('/api/status', (req: Request, res: Response) => {
   res.send('Ok');
