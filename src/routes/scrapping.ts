56de34cfcb13fb4532417a87322db90e7f37a7e4
Lautaro Bustos <lautigbustos@gmail.com>
2021-08-27 14:44:18 -0300

    fix bug

--- src/routes/scrapping.ts
+++ src/routes/scrapping.ts
@@ -1,21 +1,25 @@
 import express, { Application, Request, Response } from 'express';
 import getAllImages from '../services/scrapping';
 
-const scrappingAPI = (app: Application): void => {
+const extractAPI = (app: Application): void => {
   const router = express.Router();
   app.use('/api/extract', router);
 
   router.post('/', (req: Request, res: Response) => {
     const weburl = req.body.weburl;
-    getAllImages(weburl)
-      .then((images) => {
-        return res.status(200).json({ images });
-      })
-      .catch((error) => {
-        console.log(error);
-        return res.json({ images: [], error: true });
-      });
+    if (weburl) {
+      getAllImages(weburl)
+        .then((images) => {
+          return res.status(200).json({ images });
+        })
+        .catch((error) => {
+          console.log(error);
+          return res.json({ images: [], error: true });
+        });
+    } else {
+      return res.status(400).json({ images: [], error: true });
+    }
   });
 };
 
-export default scrappingAPI;
+export default extractAPI;
