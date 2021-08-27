b9fa1e3fd0696c7d7ad93f8ece90f3100fbfca21
Lautaro Bustos <lautigbustos@gmail.com>
2021-08-27 10:03:05 -0300

    added more image info

--- src/services/download.ts
+++ src/services/download.ts
@@ -1,23 +1,29 @@
 import request from 'request';
-import fs from 'fs';
+import { getImageInfo } from './scrapping';
 
 interface DownloadedImage {
   image: any;
   contentType: string;
+  type: string;
+  name: string;
+  fileName: string;
 }
 
 export const downloadImage = (url: string): Promise<DownloadedImage> => {
   return new Promise((resolve, reject) => {
     request({ url: url, encoding: null }, (error, res, body) => {
+      const imageInfo = getImageInfo(url);
       if (!error && res.statusCode == 200) {
-        resolve({ image: body, contentType: res.headers['content-type'] });
+        resolve({
+          image: body,
+          contentType: res.headers['content-type'],
+          type: imageInfo.type,
+          name: imageInfo.name,
+          fileName: imageInfo.fileName,
+        });
       } else {
         reject(error);
       }
     });
   });
-};
-
-export const test = (url: string) => {
-  request(url).pipe(fs.createWriteStream('test1.png'));
 };
