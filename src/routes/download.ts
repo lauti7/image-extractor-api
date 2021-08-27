5f259778c716578ecaf9c6afdb6ee6eba5a8cdde
Lautaro Bustos <lautigbustos@gmail.com>
2021-08-27 10:02:11 -0300

    added forceDownload

--- src/routes/download.ts
+++ src/routes/download.ts
@@ -8,12 +8,28 @@ const downloadAPI = (app: Application): void => {
 
   router.get('/single', (req: Request, res: Response) => {
     const weburl = req.query.weburl as string;
+
     downloadImage(weburl)
       .then((downloadedImage) => {
-        console.log('Downloaded Image: ', downloadedImage);
-        res.set({ 'Content-Type': downloadedImage.contentType });
-        res.set({ 'Content-Length': downloadedImage.image.length });
-        return res.send(downloadedImage.image);
+        if (!req.query.forceDownload) {
+          res.set({ 'Content-Type': downloadedImage.contentType });
+          res.set({ 'Content-Length': downloadedImage.image.length });
+          return res.send(downloadedImage.image);
+        } else {
+          const { type, fileName, name } = downloadedImage;
+
+          const fileContent = Buffer.from(downloadedImage.image, 'base64');
+          const readStream = new PassThrough();
+          readStream.end(fileContent);
+
+          res.set(
+            'Content-disposition',
+            'attachment; filename=' + `${name}.${type}`
+          );
+          res.set('Content-Type', 'text/plain');
+
+          readStream.pipe(res);
+        }
       })
       .catch((error) => {
         return res.status(500);
